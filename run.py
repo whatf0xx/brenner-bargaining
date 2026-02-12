a83ca5e32ae5787bcbc10866aaecf4431b93ff19
Harry Fox <hfox022@gmail.com>
2026-02-12 21:47:44 +0100

    Smooth dynamics based on gradient movement but doesn't show consistent repression etc.

--- run.py
+++ run.py
@@ -10,14 +10,17 @@ class Agent:
     rho: float
     v: float
 
-    def util(self, other: "Agent") -> float:
+    def threat(self, other: "Agent") -> float:
         p_i, rho_i, v_i  = self.p, self.rho, self.v
         p_j, rho_j, v_j = other.p, other.rho, other.v
-        threat = rho_i * p_i / v_j
-        nash = 0.5 * (p_i * (1 + rho_i / v_j) \
-                    + p_j * (1 - rho_j / v_i))
+        return rho_i * p_i / v_j
 
-        return max(threat, nash)
+    def nash(self, other: "Agent") -> float:
+        p_i, rho_i, v_i  = self.p, self.rho, self.v
+        p_j, rho_j, v_j = other.p, other.rho, other.v
+        return 0.5 * (p_i * (1 + rho_i / v_j) \
+                    + p_j * (1 - rho_j / v_i))
+        
 
     def dudp(self, other: "Agent") -> float:
         rho_i = self.rho
@@ -35,19 +38,28 @@ class Agent:
         return 0.5 * rho_j * p_j / v_i**2
 
     def invest(self, other: "Agent", i: float):
-        options = {
-            "p": self.dudp(other),
-            "rho": self.dudrho(other),
-            "v": self.dudv(other),
-        }
-        pick = max(options, key=lambda k: options[k])
-        new_val = getattr(self, pick) + i
-        setattr(self, pick, new_val)
+        dudp = self.dudp(other)
+        dudrho = self.dudrho(other)
+        dudv = self.dudv(other)
+        tot = dudp + dudrho + dudv
+        self.p += i * dudp / tot
+        self.rho += i * dudrho / tot
+        self.v += i * dudv / tot
 
 
 def step(peasant: Agent, elite: Agent, scale: float):
-    peasant_u = peasant.util(elite)
-    elite_u = elite.util(peasant)
+    peasant_nash = peasant.nash(elite)
+    elite_nash = elite.nash(peasant)
+    peasant_threat = peasant.threat(elite)
+    elite_threat = elite.threat(peasant)
+
+    if peasant_nash > peasant_threat and elite_nash > elite_threat:
+        peasant_u = peasant_nash
+        elite_u = elite_nash
+    else:
+        peasant_u = peasant_threat
+        elite_u = elite_threat
+
     peasant.invest(elite, peasant_u * scale)
     elite.invest(peasant, elite_u * scale)
 
@@ -62,26 +74,26 @@ if __name__ == "__main__":
     elite = Agent(
         0.3,
         0.2,
-        0.2,
+        0.3,
     )
 
     peasants = [deepcopy(peasant)]
     elites = [deepcopy(elite)]
-    p_u = [peasant.util(elite)]
-    e_u = [elite.util(peasant)]
+    # p_u = [peasant.util(elite)]
+    # e_u = [elite.util(peasant)]
     t = range(100)        
     for _ in t:
-        p_u.append(peasant.util(elite))
-        e_u.append(elite.util(peasant))
-        step(peasant, elite, 0.01)
+        # p_u.append(peasant.util(elite))
+        # e_u.append(elite.util(peasant))
+        step(peasant, elite, 0.02)
         peasants.append(Agent(peasant.p, peasant.rho, peasant.v))
         elites.append(Agent(elite.p, elite.rho, elite.v))
 
-    plt.figure()
-    plt.title("Utility")
-    plt.plot(p_u, label="peasant")
-    plt.plot(e_u, label="elite")
-    plt.legend()
+    # plt.figure()
+    # plt.title("Utility")
+    # plt.plot(p_u, label="peasant")
+    # plt.plot(e_u, label="elite")
+    # plt.legend()
 
     plt.figure()
     plt.title("Productivity")
