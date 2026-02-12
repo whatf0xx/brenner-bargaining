b5365e7cdb69097d2fb7b7097a4c42ed5f517f1c
Harry Fox <hfox022@gmail.com>
2026-02-12 22:02:42 +0100

    smooth but not super interesting, nice to plot agreement

--- run.py
+++ run.py
@@ -11,8 +11,8 @@ class Agent:
     v: float
 
     def threat(self, other: "Agent") -> float:
-        p_i, rho_i, v_i  = self.p, self.rho, self.v
-        p_j, rho_j, v_j = other.p, other.rho, other.v
+        p_i, rho_i  = self.p, self.rho
+        v_j = other.v
         return rho_i * p_i / v_j
 
     def nash(self, other: "Agent") -> float:
@@ -39,7 +39,7 @@ class Agent:
 
     def invest(self, other: "Agent", i: float):
         dudp = self.dudp(other)
-        dudrho = self.dudrho(other)
+        dudrho = self.dudrho(other) * 0.1
         dudv = self.dudv(other)
         tot = dudp + dudrho + dudv
         self.p += i * dudp / tot
@@ -53,7 +53,8 @@ def step(peasant: Agent, elite: Agent, scale: float):
     peasant_threat = peasant.threat(elite)
     elite_threat = elite.threat(peasant)
 
-    if peasant_nash > peasant_threat and elite_nash > elite_threat:
+    agree = peasant_nash > peasant_threat and elite_nash > elite_threat
+    if agree:
         peasant_u = peasant_nash
         elite_u = elite_nash
     else:
@@ -62,38 +63,61 @@ def step(peasant: Agent, elite: Agent, scale: float):
 
     peasant.invest(elite, peasant_u * scale)
     elite.invest(peasant, elite_u * scale)
+    return peasant_u, elite_u, agree
 
 
 if __name__ == "__main__":
     peasant = Agent(
-        0.6,
-        0.2,
+        0.9,
         0.1,
+        0.6,
     )
 
     elite = Agent(
-        0.3,
         0.2,
-        0.3,
+        0.05,
+        1.4,
     )
 
     peasants = [deepcopy(peasant)]
     elites = [deepcopy(elite)]
-    # p_u = [peasant.util(elite)]
-    # e_u = [elite.util(peasant)]
+    peasant_nash = peasant.nash(elite)
+    elite_nash = elite.nash(peasant)
+    peasant_threat = peasant.threat(elite)
+    elite_threat = elite.threat(peasant)
+
+    if peasant_nash > peasant_threat and elite_nash > elite_threat:
+        peasant_u = peasant_nash
+        elite_u = elite_nash
+        agreements = [True]
+    else:
+        peasant_u = peasant_threat
+        elite_u = elite_threat
+        agreements = [False]
+
+    p_u = [peasant_u]
+    e_u = [elite_u]
     t = range(100)        
     for _ in t:
-        # p_u.append(peasant.util(elite))
-        # e_u.append(elite.util(peasant))
-        step(peasant, elite, 0.02)
+        peasant_u, elite_u, agree = step(peasant, elite, 0.02)
+        agreements.append(agree)
+        p_u.append(peasant_u)
+        e_u.append(elite_u)
         peasants.append(Agent(peasant.p, peasant.rho, peasant.v))
         elites.append(Agent(elite.p, elite.rho, elite.v))
 
-    # plt.figure()
-    # plt.title("Utility")
-    # plt.plot(p_u, label="peasant")
-    # plt.plot(e_u, label="elite")
-    # plt.legend()
+    plt.figure()
+    plt.title("Utility")
+    plt.plot(p_u, label="peasant")
+    plt.plot(e_u, label="elite")
+    for ti, agreement, p, e in zip(t, agreements, p_u, e_u):
+        if agreement:
+            plt.plot(ti, p, "bo", alpha=0.3)
+            plt.plot(ti, e, "bo", alpha=0.3)
+        else:
+            plt.plot(ti, p, "ro", alpha=0.3)
+            plt.plot(ti, e, "ro", alpha=0.3)
+    plt.legend()
 
     plt.figure()
     plt.title("Productivity")
